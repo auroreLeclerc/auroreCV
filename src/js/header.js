502d637861c14125aeb4e29305519b274708d067
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2022-10-30 16:53:17 +0100

    BeforeInstallPromptEvent

--- src/js/header.js
+++ src/js/header.js
@@ -1,22 +1,22 @@
-const header = document.querySelector("body header")
+const header = document.querySelector("body header");
 header.insertAdjacentHTML("afterbegin", `<div id="airplane">✈️</div>`);
 const airplane = document.getElementById("airplane");
 
-const offline = () => {
+const OFFLINE = () => {
     airplane.style.top = "5px";
     airplane.style.left = "5px";
 },
-online = () => {
+ONLINE = () => {
     airplane.style.top = "2em";
     airplane.style.left = "-2em";
 };
 
-navigator.onLine ? online() : offline();
-addEventListener("offline", () => offline());
-addEventListener("online", () => online());
+navigator.onLine ? ONLINE() : OFFLINE();
+addEventListener("offline", OFFLINE);
+addEventListener("online", ONLINE);
 
 if ("share" in navigator) {
-    header.insertAdjacentHTML("beforeend", `<h1 id="share"><a>Partager !</a></h1>`);
+    header.insertAdjacentHTML("beforeend", `<h1 id="share"><a href="#top">Partager !</a></h1>`);
     document.getElementById("share").addEventListener("click", () => {
         navigator.share({
             title: "Partager le Curriculum vitæ d'Aurore Leclerc",
@@ -29,8 +29,29 @@ if ("share" in navigator) {
 }
 
 if ("print" in window && (window.location.pathname.endsWith("/index.html") || window.location.pathname.endsWith("/"))) {
-    header.insertAdjacentHTML("beforeend", `<h1 id="print"><a>Imprimer</a></h1>`);
+    header.insertAdjacentHTML("beforeend", `<h1 id="print"><a href="#top">Imprimer</a></h1>`);
     document.getElementById("print").addEventListener("click", () => {
         window.print();
+    });
+}
+
+if ("BeforeInstallPromptEvent" in window) {
+    header.insertAdjacentHTML("beforeend", `<h1 id="install"><a href="#top">Installer</a></h1>`);
+    const install = document.getElementById("install");
+    install.style.display = "none";
+    let deferredPrompt = null;
+
+    install.addEventListener("click", () => {
+        install.style.display = "none";
+        deferredPrompt.prompt().then(choice => {
+            console.info(`${choice.platforms} user ${choice.outcome} the A2HS prompt`);
+            deferredPrompt = null;
+        });
+    });
+
+    window.addEventListener("beforeinstallprompt", (event) => {
+        event.preventDefault();
+        deferredPrompt = event;
+        install.style.display = "block";
     });
 }
