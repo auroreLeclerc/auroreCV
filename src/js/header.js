ef7ed1001249646b67087d805cd344d1b3b87b36
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2023-02-14 11:42:15 +0100

    og:image

--- src/js/header.js
+++ src/js/header.js
@@ -27,7 +27,7 @@ if ("share" in navigator) {
 		navigator.share({
 			title: "Partager le Curriculum vitæ d'Aurore Leclerc",
 			text: "Je t'ai montré le CV d'Aurore Leclerc ?\nRegarde il peut même être installé sur ton appareil (en PWA) !",
-			url: "https://auroreleclerc.github.io/auroreCV/"
+			url: "https://auroreleclerc.github.io/auroreCV/index.html"
 		}).then(() => {
 			console.info("Shared !");
 		}).catch(error => {
