276951827f32a3192d0de39dc07571ad3b33c694
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2023-08-12 12:54:20 +0200

    back

--- service-worker.js
+++ service-worker.js
@@ -36,7 +36,7 @@ self.addEventListener("install", function(/** @type {ExtendableEvent} */ event) 
 									state: "success",
 									total: urls.length - 1,
 									done: done++
-								});
+								});	  
 							}).catch(error =>{
 								console.error("📪", error.message, url);
 								channel.postMessage({
@@ -45,10 +45,6 @@ self.addEventListener("install", function(/** @type {ExtendableEvent} */ event) 
 									total: urls.length - 1,
 									done: done++
 								});
-							}).finally(() => {
-								if (done >= urls.length - 1) {
-									channel.close();
-								}	  
 							});
 						}
 					});
