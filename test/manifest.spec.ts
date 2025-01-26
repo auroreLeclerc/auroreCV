110425b6b454626942a84749fafe9a565b858759
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2025-01-27 00:16:40 +0100

    continuity

--- test/manifest.spec.ts
+++ test/manifest.spec.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from "node:fs";
 import manifest from "./www/manifest.json" with { type: "json" };
 import assert from "node:assert";
 
@@ -5,6 +6,16 @@ describe("manifest.json", function () {
 	context("Syntax", function () {
 		it("Semantic Versioning", function () {
 			assert.ok(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/.test(manifest.version));
+		});
+	});
+	context.skip("Continuity", function () {
+		let packageJson: any;
+		before(function() {
+			packageJson = readFileSync("../package.json");
+		});
+		it("V4 upgrade bypass removed", function () {
+			assert.ok(typeof packageJson.version !== "string");
+			assert.ok(!Array.isArray(packageJson.changelogs));
 		});
 	});
 });
